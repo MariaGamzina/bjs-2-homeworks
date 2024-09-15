48fbf9d1538bd12d922e1b3ef73fee781a80fc90
Maria Gamzina <gamzinamaria@gmail.com>
2024-09-15 22:13:42 +0300

    base concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+	if (a === 0) {
+		let x = -c / b;
+		arr.push(x);
+		return arr;
+	} else if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+		return arr;
+	} else if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1);
+		arr.push(x2);
+		return arr;
+	}
+
 }
+console.log(arr);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let p = percent / 100 / 12;
+	let s = amount - contribution;
+	let total = 0;
+	if (s > 0) {
+		let payment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+		let allPercent = payment * countMonths - amount;
+		total = +(contribution + amount + allPercent).toFixed(2);
+		return total;
+	} else return total;
 }
+console.log(total);

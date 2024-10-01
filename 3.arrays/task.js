3311c63ee4334c3e1b03f2ca2963bd99fc12d3bb
Maria Gamzina <gamzinamaria@gmail.com>
2024-10-01 14:28:39 +0300

    task3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((value, index) => {
+		console.log(value, arr2[index]);
+		return value === arr2[index];
+	})
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filterGender = users.filter(user => user.gender === gender);
+	if (filterGender.length === 0) {
+		return 0;
+	}
+
+	const ages = filterGender.map(user => user.age);
+	const averageAge = (ages.reduce((acc, age) => (acc + age), 0)) / filterGender.length;
+	return averageAge;
+
 }
